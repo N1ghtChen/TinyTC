6346d01e2b35463f66ef5271751d497d3d0cb206
N1ghtChen <night_chen0521@sina.cn>
2019-06-04 17:52:10 +0800

    add scrapy framework

---
+++ crawler/WpageSpider/WpageSpider/spiders/wpage_spider.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class WpageSpider(scrapy.spiders):
+    name = "webpage"
+
+    def __init__(self):
+        pass
+    def parse(self, response):
+        pass
+
+    pass
